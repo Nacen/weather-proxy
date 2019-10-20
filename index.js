40449a16cb21b81d029d3aeaa14ec8e18a37f62a
Vincent <overlordnacen@gmail.com>
2019-10-20 11:41:12 +0800

    change header settings

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "GET,PUT,POST,DELETE,PATCH,OPTIONS"
   );
-  res.header("Access-Control-Allow-Headers", "Content-Type, content-type");
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
 });
 
